137a60bda3e0103f5f30895df10d04df95e751f6
tatemouser <tatesmouser@gmail.com>
2024-06-15 12:12:52 -0500

    Split video for compression

--- public/Assets/js/script.js
+++ public/Assets/js/script.js
@@ -2,3 +2,20 @@ function toggleDropdown() {
     var navLinks = document.getElementById("navLinks");
     navLinks.classList.toggle("show");
   }
+
+  document.addEventListener('DOMContentLoaded', (event) => {
+    const video1 = document.getElementById('video1');
+    const video2 = document.getElementById('video2');
+
+    video1.addEventListener('ended', () => {
+        video1.style.display = 'none';
+        video2.style.display = 'block';
+        video2.play();
+    });
+
+    video2.addEventListener('ended', () => {
+        video2.style.display = 'none';
+        video1.style.display = 'block';
+        video1.play();
+    });
+});
